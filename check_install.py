c700514b7aebfd982addae163a4db83d91f63975
Eelon Lappalainen <eelon.lappalainen@alumni.aalto.fi>
2026-06-06 14:15:35 +0300

    Add files via upload

---
+++ check_install.py
@@ -0,0 +1,3 @@
+from bim.ifc_reader import read_ifc_elements, diagnose_ifc, diagnose_materials
+from model.simulation import run_simulation, summarize, normalize_elements, filter_elements, aggregate_elements, status_for_day, progress_by_day
+print('OK: imports succeeded')
